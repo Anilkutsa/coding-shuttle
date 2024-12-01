e4e321a33c29c5c3625d719bd6165a317cb11117
Anil Suryaprakash <anilkutsa@gmail.com>
2024-12-01 18:45:20 +0530

    Delete W2P2-Department directory

--- W2P2-Department/src/main/java/com/codingshuttle/w2p2/department/w2p2_department/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codingshuttle.w2p2.department.w2p2_department.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
